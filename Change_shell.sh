9060590fcdb256090aab54719aec96330a322bbf
BLE22H <56558093+Tool2020@users.noreply.github.com>
2020-07-21 22:24:11 +0630

    Add files via upload

---
+++ Change_shell.sh
@@ -0,0 +1,38 @@
+
+clear
+
+read -p $'\e[32mTermux root@.... :' style
+
+read -p $'\e[32m Termux banner :' banner
+
+read -p $'\e[32mEnter Name title :\e[31m' Name
+
+clear
+
+cd
+
+cd ..
+
+cd usr/etc
+
+rm motd
+
+rm bash.bashrc
+
+cat <<LOGIN>bash.bashrc
+
+echo -e "\e[96m<<$Name>>\e[109m"
+
+ figlet          $banner
+
+PS1='\033[32m\a╔═\a║\d║════║\@║═\a═══════╗\a\a
+║ \a\a\a
+╚\a════║root@\e[31m$style\e[32m║══\a═══║\#\e[180m\w\e[32m║══════\a>'
+
+
+
+
+
+
+
+
